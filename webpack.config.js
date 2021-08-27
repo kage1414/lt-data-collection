5f91de30e5e4f15fde9747e116645c6b4fc513d3
kage1414 <kylejohnson92294@gmail.com>
2021-08-27 18:16:48 -0400

    Exclude node modules webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
       {
         test: /\.jsx$/,
         include: path.resolve(__dirname, 'client'),
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
         }
@@ -20,12 +21,14 @@ module.exports = {
       {
         test: /\.js$/,
         include: path.resolve(__dirname, 'client'),
+        exclude: /node_modules/,
         enforce: 'pre',
         use: ['babel-loader', 'source-map-loader']
       },
       {
         test: /\.css$/i,
         include: path.resolve(__dirname, 'client'),
+        exclude: /node_modules/,
         use: ['style-loader', 'css-loader'],
       }
     ]
